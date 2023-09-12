d9626c4b6077812cbf716d81db216ab62a014913
Yasashri <ymedagedara@gmail.com>
2023-09-12 15:03:40 +0530

    Created update and delete methods

--- main.py
+++ main.py
@@ -22,12 +22,30 @@ def get_game(game_id):
             return game
         return {'error':'Game not found'}
     
-#Create a new game entry
+#Create a new entry
 @app.route('/games', methods=['POST'])
 def create_game():
     new_game = {'id':len(games)+1,'title':request.json['title'], 'developer':request.json['developer']}
-    games.appened(new_game)
+    games.append(new_game)
     return new_game
+
+#Update an entry
+@app.route('/games/<int:game_id>', methods=['PUT'])
+def update_games(game_id):
+    for game in games:
+        if game['id']==game_id:
+            game['title']=request.json['title']
+            game['developer']=request.json['developer']
+            return game
+        return {'error':'Game not found'}
+    
+#Delete an enrty
+@app.route('/games/<int:game_id>', methods=['DELETE'])
+def delete_game(game_id):
+    for game in games:
+        if game['id']==game_id:
+            return game
+        return {'error': 'Game not found'}
 
 #running the API
 if __name__ == '__main__':
