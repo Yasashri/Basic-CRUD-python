091b0f5584c9db022b1cc0fd9ea52e77ff9a29fe
Yasashri <ymedagedara@gmail.com>
2023-09-12 12:13:31 +0530

    Created get route

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ games = [
     {"id":2,"title":"Call of Duty", "developer":"Activision"},
     {"id":3,"title":"Red Dead Redemption", "developer":"Rockstar Games"}
 ]
+
+#Get all games
+app.route('/games', setMethod=['GET'])
+def get_games():
+    return games
+
+#running thid API
+if __name__ == '__main__':
+    app.run(debug=True)
